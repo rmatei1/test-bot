fc98b7f07d6fbf7fd0b36da6253d6721b8cb4064
Rares Matei <matei.rar@gmail.com>
2021-01-26 11:14:26 +0000

    wip3

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+let a = "2";
+a = 5;
+console.log(a)
+
 @Module({
   imports: [],
   controllers: [AppController],
